dc9ab4fbf1a337554c5b5daa58562b10a81c44b0
Mau <mpino1701@gmail.com>
2022-09-12 23:52:56 -0300

    se genera clase de registro de notas

---
+++ entidades/registro_notas_empresas.py
@@ -0,0 +1,20 @@
+
+
+class Registro_Notas_Empresas():
+    def __init__(self,**kargs):
+        self.id_registro = kargs.get("id_registro")
+        self.rut_empresa = kargs.get("rut_empresa")
+        self.correo = kargs.get("correo")
+        self.fecha_creacion = kargs.get("fecha_creacion")
+        
+    def preparar(self):
+        return {"estado":"registro_notas_empresas", "contenido":self}
+    
+    def __str__(self):
+        return '''
+    ID Registro: {}
+    Rut Empresa: {}
+    Correo: {}
+    Fecha Creacion {}
+    '''.format(self.id_registro, self.rut_empresa,
+               self.correo, self.fecha_creacion)
