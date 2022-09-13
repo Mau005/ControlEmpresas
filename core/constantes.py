5da278bd6c06063b1ae02927dfd33c35db4cb139
Mau <mpino1701@gmail.com>
2022-09-12 23:52:38 -0300

    soporte de error y confirmacion

--- core/constantes.py
+++ core/constantes.py
@@ -6,4 +6,9 @@ BUTTONCREATE = {'Crear': 'pencil', 'Formatear': 'delete', 'Salir': 'exit-run'}
 PROTOCOLOERROR = {"CUENTA": "Correo o Usuario son incorrectos",
                 "INSERTAR": "Información dada no se puede almacenar",
                 "NETWORK": "Sin conexión con el Servidor",
-                "LONGITUD": "La cantidad de caracteres es incorrecta"}
+                "LONGITUD": "La cantidad de caracteres es incorrecta",
+                "ERROR": "Error Inesperado, no se entiende aun la falla Error Fatal.",
+                "FECHAS": "Necesita una fecha almenos."}
+PROTOCOLOAFIRMATIVO = {
+    "INSERTAR": "Información almacenada con exito"
+}
