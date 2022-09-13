9b4d75ff747dbac272b12b7bd5b56152cc39cd7d
Mau <mpino1701@gmail.com>
2022-09-12 23:54:16 -0300

    se agregan querys de registro de empresas

--- schema/querys.py
+++ schema/querys.py
@@ -48,9 +48,25 @@ class Querys():
         return self.bd.insertar(querys)
         
     def registrar_servicios(self,datos):
-        querys = f'''
-        INSERT INTO SERVICIOS(NOMBRE_SERVICIO, DESCRIPCION, FECHA_INICIO, FECHA_TERMINO, PRECIO, ID_ESTADO)
-        VALUES("{datos.get("nombre")}", "{datos.get("descr")}","{datos.get("fecha_inicio")}","{datos.get("fecha_termino")}", {int(datos.get("precio"))}, {int(datos.get("id_estado"))});
-        '''
+        fecha_termino = datos.get("fecha_termino")
+        if fecha_termino == "NULL":
+            querys = f'''
+            INSERT INTO SERVICIOS(NOMBRE_SERVICIO, DESCRIPCION, FECHA_INICIO, PRECIO, ID_ESTADO)
+            VALUES("{datos.get("nombre")}", "{datos.get("descr")}","{datos.get("fecha_inicio")}", {int(datos.get("precio"))}, {int(datos.get("id_estado"))});
+            '''
+        else:
+            querys = f'''
+            INSERT INTO SERVICIOS(NOMBRE_SERVICIO, DESCRIPCION, FECHA_INICIO, FECHA_TERMINO, PRECIO, ID_ESTADO)
+            VALUES("{datos.get("nombre")}", "{datos.get("descr")}","{datos.get("fecha_inicio")}","{fecha_termino}", {int(datos.get("precio"))}, {int(datos.get("id_estado"))});
+            '''
         
+        return self.bd.insertar(querys)
+    
+    def registrar_empresas(self, objeto):
+        
+        querys = '''
+        INSERT INTO EMPRESAS(RUT_EMPRESA, NOMBRE_EMPRESA, GIRO_EMPRESA, DIRECCION, TELEFONO, CORREO_EMPRESA, CORREO_RESPALDO, CELULAR_EMPRESA)
+        VALUES("{0}", "{1}", "{2}", "{3}", "{4}", "{5}" , "{6}", "{7}");
+        '''.format(objeto.rut_empresa, objeto.nombre_empresa, objeto.giro_empresa,
+                   objeto.direccion_empresa, objeto.telefono, objeto.correo_empresa, objeto.correo_respaldo, objeto.celular_empresa)
         return self.bd.insertar(querys)
