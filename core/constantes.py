99599795a74c0568bfd8e26fd223ff20541a4c2d
mauro <mpino1701@gmail.com>
2022-09-28 04:11:00 -0300

    se prepara para un calendario interno

--- core/constantes.py
+++ core/constantes.py
@@ -17,3 +17,5 @@ TIEMPOACTUALIZAR = 1#segundos
 REVISARCORREOS = 2
 TIMEPOESPERAUSUARIO = 60*5
 TIEMPOESPERADIGITO = 60 * 5
+
+DIA_SEMANA = { 1: "Lunes", 2: "Martes", 3:"Miercoles", 4:"Jueves", 5: "Viernes", 6: "Sabado", 7: "Domingo"}
--- test.py
+++ test.py
@@ -1,12 +1,25 @@
+from datetime import datetime
+from core.constantes import DIA_SEMANA
 
 
-def comprobar(args):
-    test = """
-    Hola: {0}
-    Hola: {1}
-    Hola: {2}
-    """
-    return test.format(args)
 
-print(comprobar())
+class Calendario_Notificaciones():
+    def __init__(self):
+        self.dia_actual = DIA_SEMANA[datetime.isoweekday(datetime.now())]
 
+    def consultar_dia(self, año, mes, dia):
+        return DIA_SEMANA[datetime.isoweekday(datetime(año, mes, dia))]
+
+    def consultar_dia_isoformato(self, año, mes, dia):
+        return datetime(año, mes, dia).isoformat()
+
+    def consultar_fechas(self,date1, date2):
+        print(date1-date2)
+
+    def actualizar(self, dt):
+        self.dia_actual = DIA_SEMANA[datetime.isoweekday(datetime.now())]
+
+if __name__ == "__main__":
+    test = Calendario_Notificaciones()
+    print(test.dia_actual)
+    print(test.consultar_fechas(datetime(2021,9,25), datetime(2022,9,30)))
