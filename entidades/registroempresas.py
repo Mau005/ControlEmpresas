f3ecaaa0d7616b1176223aca49c633cd1a57b53a
Mau <mpino1701@gmail.com>
2022-10-11 05:59:07 -0300

    se repara telefono de registro en empresas

--- entidades/registroempresas.py
+++ entidades/registroempresas.py
@@ -1,19 +1,20 @@
 
 
 class RegistroEmpresas:
-    
+    #rut_empresa	nombre_empresa	giro_empresa	direccion_empresa
+    # correo_empresa	correo_respaldo	telefono_empresa	celular_empresa
     def __init__(self, **kargs):
         self.rut_empresa = kargs.get("rut_empresa")
         self.nombre_empresa = kargs.get("nombre_empresa")
         self.giro_empresa = kargs.get("giro_empresa")
         self.direccion_empresa = kargs.get("direccion_empresa")
-        self.telefono = kargs.get("telefono")
+        self.telefono_empresa = kargs.get("telefono_empresa")
         self.correo_empresa = kargs.get("correo_empresa")
         self.correo_respaldo = kargs.get("correo_respaldo")
         self.celular_empresa = kargs.get("celular_empresa")
         
     def preparar(self):
-        return {"estado": "registroempresa", "contenido":self}
+        return {"estado": "registro_empresa", "contenido":self}
         
     def __str__(self):
         return '''
@@ -26,7 +27,7 @@ class RegistroEmpresas:
     Correo Respaldo: {}
     Celular empresa: {}
     '''.format(self.rut_empresa, self.nombre_empresa, self.giro_empresa,
-               self.direccion_empresa, self.telefono, self.correo_empresa,
+               self.direccion_empresa, self.telefono_empresa, self.correo_empresa,
                self.correo_empresa, self.celular_empresa)
 
         
