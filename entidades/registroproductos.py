3d7ed10d62b6882bb161ad52aae603f3aa85be20
Mau <mpino1701@gmail.com>
2022-10-11 05:59:20 -0300

    se agrega locales en productos

--- entidades/registroproductos.py
+++ entidades/registroproductos.py
@@ -1,13 +1,14 @@
 
 
 class RegistroProductos:
-    
+    #	id_producto	nombre_producto	descripcion	fecha_creacion	cantidad
     def __init__(self, **kargs):
         self.id_producto = kargs.get("id_producto")
         self.nombre_producto = kargs.get("nombre_producto")
         self.descripcion = kargs.get("descripcion")
         self.fecha_creacion = kargs.get("fecha_creacion")
         self.cantidad = kargs.get("cantidad")
+        self.id_local = kargs.get("id_local")
         
     def __str__(self):
         return """
@@ -17,7 +18,8 @@ class RegistroProductos:
     Descripcion: {}
     Fecha_Creacion: {}
     cantidad: {}
-    """.format(self.id_producto, self.nombre_producto, self.descripcion, self.fecha_creacion,self.cantidad)
+    Local: {}
+    """.format(self.id_producto, self.nombre_producto, self.descripcion, self.fecha_creacion,self.cantidad, self.id_local)
     
     def preparar(self):
-        return {"estado": "registroproductos", "contenido": self}
+        return {"estado": "registro_productos", "contenido": self}
