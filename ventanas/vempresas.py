abda53946c26f8257a9cbff180f39f48aecd354e
Mau <mpino1701@gmail.com>
2022-10-31 20:56:33 -0300

    se separa un objeto de tipo empresa del resto de interfaz, para reutilizar todo sus widgets

--- ventanas/vempresas.py
+++ ventanas/vempresas.py
@@ -1,4 +1,5 @@
 from core.constantes import PROTOCOLOERROR
+from ventanas.empresa import Empresa
 from ventanas.widgets_predefinidos import MDScreenAbstrac, Notificacion
 from entidades.registroempresas import RegistroEmpresas
 from core.herramientas import Herramientas as her
@@ -6,72 +7,39 @@ from kivy.logger import Logger
 
 
 class VEmpresas(MDScreenAbstrac):
-
     def __init__(self, network, manejador, nombre, siguiente=None, volver=None, **kw):
         super().__init__(network, manejador, nombre, siguiente, volver, **kw)
         self.correo = "prueba"
+        self.empresa = Empresa()
+        self.ids.contenedor_principal.add_widget(self.empresa)
 
     def crear(self, *args):
-        longitud = 1
-        noti = Notificacion("Error", "")
-        estado = True
-        if not (9 <= len(self.ids.rut_empresa.text) <= 10):
-            noti.text += "Debe tener entre 9 y 10 caracteres el RUT de Empresa ejemplo xxxxxxxx-x\n"
-            estado = False
-        if not (len(self.ids.nombre_empresa.text) >= longitud):
-            noti.text += "Debe tener Contenido el Nombre de Empresa\n"
-            estado = False
-        if not (len(self.ids.giro_empresa.text) >= longitud):
-            noti.text += "Debe tener Contenido el Giro de la empresa\n"
-            estado = False
-        if not (len(self.ids.direccion_empresa.text) >= longitud):
-            noti.text += "Debe tener contenido la direccion de la empresa\n"
-            estado = False
-        if not (len(self.ids.celular_empresa.text) >= 8):
-            noti.text += "El celular de empresa debe tener almenos 8 numeros\n"
-            estado = False
-        if not ("@" in self.ids.correo_empresa.text):
-            noti.text += "El Correo de la empresa debe tener almenos un @\n"
-            estado = False
-        if not estado:
-            noti.open()
-            return None
-        verificando_rut = her.verificar_rut(self.ids.rut_empresa.text)
+        if not self.empresa.crear():
+            return
 
-        if verificando_rut[0]:
-            objeto = RegistroEmpresas(
-                rut_empresa=verificando_rut[1],
-                nombre_empresa=self.ids.nombre_empresa.text,
-                giro_empresa=self.ids.giro_empresa.text,
-                direccion_empresa=self.ids.direccion_empresa.text,
-                telefono_empresa=self.ids.telefono_empresa.text,
-                celular_empresa=self.ids.celular_empresa.text,
-                correo_empresa=self.ids.correo_empresa.text,
-                correo_respaldo=self.ids.correo_respaldo_empresa.text,
-            )
-            self.network.enviar(objeto.preparar())
-            info = self.network.recibir()
-            if info.get("estado"):
-                Logger.info("Se ha creado el dato empresa")
-                noti = Notificacion("Exito", f"Se ha generado con exito la Empresa {objeto.nombre_empresa}")
-                noti.open()
-                self.formatear()
-                self.siguiente()
-                return None
-
-            noti = Notificacion("Error", PROTOCOLOERROR[info.get("condicion")])
+        verificando_rut = her.verificar_rut(self.empresa.rut_empresa.text)
+        if not verificando_rut[0]:
+            noti = Notificacion("Error", verificando_rut[1])
             noti.open()
+            return
+
+        objeto = self.empresa.generar_objeto(verificando_rut[1])
+
+        self.network.enviar(objeto.preparar())
+        info = self.network.recibir()
+
+        if info.get("estado"):
+            noti = Notificacion("Exito", f"Se ha generado con exito la Empresa {objeto.nombre_empresa}")
+            noti.open()
+            self.formatear()
             return None
+
+        noti = Notificacion("Error", PROTOCOLOERROR[info.get("condicion")])
+        noti.open()
+        return None
 
     def formatear(self, *args):
-        self.ids.rut_empresa.text = ""
-        self.ids.nombre_empresa.text = ""
-        self.ids.giro_empresa.text = ""
-        self.ids.direccion_empresa.text = ""
-        self.ids.telefono_empresa.text = ""
-        self.ids.celular_empresa.text = ""
-        self.ids.correo_empresa.text = ""
-        self.ids.correo_respaldo_empresa.text = ""
+        self.empresa.formatear()
 
     def actualizar(self, *dt):
         return super().actualizar(*dt)
