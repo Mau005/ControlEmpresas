126baa98011c329d47ee84298ab71fa1999462c3
Mau <mpino1701@gmail.com>
2023-02-18 05:57:48 -0300

    se adapta a base de datos

--- entidades/registrardepartamento.py
+++ entidades/registrardepartamento.py
@@ -1,16 +1,16 @@
 class RegistrarDepartamento:
     def __init__(self, **kargs):
-        self.id_grupo = kargs.get("id_grupo")
+        self.id_departamento = kargs.get("id_departamento")
+        self.id_local = kargs.get("id_local")
         self.nombre_departamento = kargs.get("nombre_departamento")
         self.descripcion = kargs.get("descripcion")
-        self.id_local = kargs.get("id_local")
 
-    def __str__(self):
+    def __str__(self) -> str:
         return """
         ID Grupo: {}
         Nombre Grupo: {}
         Descripcion: {}
-        """.format(self.id_grupo, self.nombre_grupo, self.desc)
+        """.format(self.id_departamento, self.nombre_departamento, self.descripcion)
 
-    def preparar(self):
+    def preparar(self) -> dict:
         return {"estado": "registrar_departamento", "contenido": self}
