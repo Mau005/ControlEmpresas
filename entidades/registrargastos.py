1a943a01e615377e4d8f3c8e69a834afffb3d92b
Mau <mpino1701@gmail.com>
2023-02-18 05:57:56 -0300

    se adapta a base de datos

--- entidades/registrargastos.py
+++ entidades/registrargastos.py
@@ -10,8 +10,9 @@ class RegistrarGastos:
         self.fecha_creacion = kargs.get("fecha_creacion")
         self.id_departamento = kargs.get("id_departamento")
         self.id_estado_gastos = kargs.get("id_estado_gastos")
+        self.rut_persona = kargs.get("rut_trabajador")
 
-    def __str__(self):
+    def __str__(self) -> str:
         return """
         ID Gasto: {}
         Descripcion: {}
@@ -19,8 +20,9 @@ class RegistrarGastos:
         Fecha Creacion: {}
         Id Departamento: {}
         id Estado Gastos: {}
+        Rut Trabajador: {}
         """.format(self.id_gasto, self.descripcion, self.saldo,
-                   self.fecha_creacion, self.id_departamento, self.id_estado_gastos)
+                   self.fecha_creacion, self.id_departamento, self.id_estado_gastos,self.rut_persona)
 
-    def preparar(self):
+    def preparar(self) -> dict:
         return {"estado":"registrar_gasto", "contenido":self}
