efa0065a8e957b22c5e598e8870929598e6f1f03
Mau <mpino1701@gmail.com>
2023-02-18 05:56:49 -0300

    se agrega ot y methodo str

--- entidades/orden_trabajos.py
+++ entidades/orden_trabajos.py
@@ -1,5 +1,4 @@
 
-
 class Orden_Trabajos:
 
     def __init__(self, **kargs):
@@ -12,15 +11,27 @@ class Orden_Trabajos:
         self.precio_ot = kargs.get("precio_ot")
         self.descripcion = kargs.get("descripcion")
 
-    def __str__(self):
+    def __str__(self) -> str:
         return f"Id Orde: {self.id_orden}" \
                f"Id Servicios: {self.id_servicios}" \
                f"descripcion: {self.descripcion}"
 
 class Orden_Trabajos_Historia:
     def __init__(self, **kargs):
+        self.id_ot_historia = kargs.get("id_ot_historia")
         self.id_orden = kargs.get("id_orden")
         self.fecha_creacion = kargs.get("fecha_creacion")
         self.id_preparativo_anterior = kargs.get("preparativo_aterior")
         self.id_preparativo_nuevo =kargs.get("preparativo_nuevo")
         self.descripcion = kargs.get("descripcion")
+
+    def __str__(self) -> str:
+        return """
+        Id Orden Historia: {}
+        Id Orden: {}
+        Fecha Creacion: {}
+        Id Preparativo Anterior: {}
+        id Preparativo Nuevo: {}
+        Descripcion: {}
+        """.format(self.id_ot_historia, self.id_orden, self.fecha_creacion, self.id_preparativo_anterior,
+                   self.id_preparativo_nuevo, self.descripcion)
