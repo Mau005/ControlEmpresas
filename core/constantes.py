92a952a84996c23f976db381f41f66b7deefb349
Mauri <mpino1701@gmail.com>
2023-02-21 16:13:09 -0300

    add protocool error not employer

--- core/constantes.py
+++ core/constantes.py
@@ -14,6 +14,7 @@ PROTOCOLOERROR = {"USUARIOACTIVO": "Usuario ya se encuentra activo en el sistema
                   "SIN_DATOS": "La consulta pedida, no tiene información",
                   "SINSELECCION":"No Se ha ingresado una seleccion correspondiente",
                   "EXPIRACION": "Se ha expirado el tiempo de seccion activa.",
-                  "NOSEENCUENTRA": "No se ha podido localizar los datos."}
+                  "NOSEENCUENTRA": "No se ha podido localizar los datos.",
+                  "NOTRABAJADOR": "Usted no pertenece al equipo de trabajo de esta empresa"}
 
 DIA_SEMANA = {1: "Lunes", 2: "Martes", 3: "Miercoles", 4: "Jueves", 5: "Viernes", 6: "Sabado", 7: "Domingo"}
