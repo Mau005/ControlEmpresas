2556d823accb053b96e3dff0a8524758eb2e83e0
Mauri <mpino1701@gmail.com>
2023-02-21 16:13:53 -0300

    add querys advance, sistem

---
+++ schema/querys_advance.py
@@ -0,0 +1,18 @@
+
+
+class Querys_Advance:
+
+    def __init__(self, bd):
+        self.bd = bd
+
+
+    def Check_Exist_Content(self, table, consult_atribute, id_atribute ) -> bool:
+        querys = """
+        SELECT
+            CASE 
+                WHEN COUNT({}) > 0 then 1 else 0
+                END as cant
+        from {}
+        WHERE {} = {}
+        """.format(consult_atribute, table, consult_atribute, id_atribute)
+        return bool(self.bd.consultar(querys)["datos"])
