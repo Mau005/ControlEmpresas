865c69c5e9645e8aede6501f95c75902ba605d26
Mauri <mpino1701@gmail.com>
2023-02-21 16:14:35 -0300

    add  id departaments

--- entidades/serviciodiarios.py
+++ entidades/serviciodiarios.py
@@ -6,11 +6,13 @@ class ServicioDiarios(AbstracServicio):
     def __init__(self, **kargs):
         super().__init__(**kargs)
         self.dias_diarios = kargs.get("dias_diarios")
+        self.id_departamento = kargs.get("id_departamento")
 
     def __str__(self):
         return super().__str__() + """
         Dias Diarios: {}
-        """.format(self.dias_diarios)
+        Id_Departamento: {}
+        """.format(self.dias_diarios, self.id_departamento)
 
     def preparar(self):
         return {"estado": "registro_servicio_diario", "contenido": self}
